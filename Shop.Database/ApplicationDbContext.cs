4f7be56d54e4a832d89819ccbd5b52ca6d21c457
ellesmate <lbdkira@gmail.com>
2020-09-16 12:54:22 +0300

    Order, OrderProduct, Stock models. Remove migrations from git and set role to .gitignore.

--- Shop.Database/ApplicationDbContext.cs
+++ Shop.Database/ApplicationDbContext.cs
@@ -10,5 +10,15 @@ namespace Shop.Database
             : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<OrderProduct>()
+                .HasKey(x => new { x.ProductId, x.OrderId });
+        }
     }
 }

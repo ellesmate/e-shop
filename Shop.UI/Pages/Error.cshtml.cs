b176acbe7a357979930f21c933457a5fe150fc69
ellesmate <lbdkira@gmail.com>
2020-09-28 12:08:43 +0300

    Handle errors.

--- Shop.UI/Pages/Error.cshtml.cs
+++ Shop.UI/Pages/Error.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,9 +13,8 @@ namespace Shop.UI.Pages
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public int Status { get; set; }
+        public string Message { get; set; }
 
         private readonly ILogger<ErrorModel> _logger;
 
@@ -23,9 +23,10 @@ namespace Shop.UI.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(int status)
         {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            Status = status != 0 ? status : Response.StatusCode;
+            Message = ((HttpStatusCode)Status).ToString();
         }
     }
 }

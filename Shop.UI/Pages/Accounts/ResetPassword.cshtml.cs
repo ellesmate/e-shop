bde13fbc35ff0101a4edba2e46309a56048b3b55
ellesmate <lbdkira@gmail.com>
2020-10-02 14:04:00 +0300

    ResetPassword

---
+++ Shop.UI/Pages/Accounts/ResetPassword.cshtml.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Internal;
+using NETCore.MailKit.Core;
+
+namespace Shop.UI.Pages.Accounts
+{
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailService _emailService;
+
+        public ResetPasswordModel(
+            UserManager<IdentityUser> userManager,
+            IEmailService emailService)
+        {
+            _userManager = userManager;
+            _emailService = emailService;
+        }
+
+        [BindProperty]
+        public ResetPasswordViewModel Input { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var user = await _userManager.FindByEmailAsync(Input.Email);
+            
+            if (user == null) return BadRequest();
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            
+            var link = Url.Action("SetPassword", "Accounts", new { userId = user.Id, code }, Request.Scheme, Request.Host.ToString());
+            
+            await _emailService.SendAsync("test@test.com", "Reset password", $"<a href=\"{link}\">Reset password</a>", true);
+
+            //_userManager.ResetPasswordAsync()
+            return RedirectToPage("/");
+        }
+
+        public class ResetPasswordViewModel
+        {
+            public string Email { get; set; }
+        }
+    }
+}
---
+++ Shop.UI/Pages/Accounts/SetPassword.cshtml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shop.UI.Pages.Accounts
+{
+    public class SetPasswordModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public SetPasswordModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [BindProperty]
+        public ResetPasswordViewModel Input { get; set; }
+        public void OnGet(string userId, string code)
+        {
+            Input = new ResetPasswordViewModel
+            {
+                UserId = userId,
+                Code = code
+            };
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var user = await _userManager.FindByIdAsync(Input.UserId);
+
+            if (user == null) return BadRequest();
+
+            var result = await _userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Accounts/Login");
+            }
+
+            return BadRequest();
+        }
+
+        public class ResetPasswordViewModel
+        {
+            public string UserId { get; set; }
+            public string Code { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}

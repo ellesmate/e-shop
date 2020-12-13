fad11b16398954055832f0fe268975015cbe2916
ellesmate <lbdkira@gmail.com>
2020-12-13 22:25:04 +0300

    Fix: external auth.

--- Shop.UI/Startup.cs
+++ Shop.UI/Startup.cs
@@ -130,21 +130,20 @@ namespace Shop.UI
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Error");
-            //    app.UseStatusCodePagesWithReExecute("/Error/{0}");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    //app.UseHsts();
-            //}
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();

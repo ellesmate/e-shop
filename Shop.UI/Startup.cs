58f48fccc57d9d38cf9c574105f35a14882db8a4
ellesmate <lbdkira@gmail.com>
2020-12-13 21:24:57 +0300

    Detail exception page.

--- Shop.UI/Startup.cs
+++ Shop.UI/Startup.cs
@@ -129,17 +129,19 @@ namespace Shop.UI
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseStatusCodePagesWithReExecute("/Error/{0}");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    //app.UseHsts();
+            //}
+            app.UseDeveloperExceptionPage();
+
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
